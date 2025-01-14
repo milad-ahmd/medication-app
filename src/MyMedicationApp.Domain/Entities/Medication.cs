543c9ed7c8b40c24542fe84a04a8c3a30296fd4b
Milad Ahmadi <miladahmadi803@gmail.com>
2025-01-15 02:17:23 +0330

    Remove files now ignored by .gitignore

--- src/MyMedicationApp.Domain/Entities/Medication.cs
+++
@@ -1,9 +0,0 @@
-namespace MyMedicationApp.Domain.Entities;
-
-public class Medication
-{
-    public Guid Id { get; set; }
-    public string Name { get; set; } = null!;
-    public int Quantity { get; set; }
-    public DateTime CreationDate { get; set; }
-}
